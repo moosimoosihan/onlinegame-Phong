c40d1ef5de1bad4bb70464a185fa9c2b62ad6bc7
moosimoosihan <92845402+moosimoosihan@users.noreply.github.com>
2023-04-05 17:44:42 +0900

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,4 +1,4 @@
-﻿using Photon.Pun;
+using Photon.Pun;
 using UnityEngine;
 
 public class Player : MonoBehaviourPun
@@ -10,11 +10,26 @@ public class Player : MonoBehaviourPun
 
     private void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(photonView.IsMine){
+            spriteRenderer.color = Color.blue;
+        } else {
+            spriteRenderer.color = Color.red;
+        }
     }
 
     private void Update()
     {
-        
+        if(!photonView.IsMine){
+            return;
+        }
+
+        var input = InputButton.VerticalInput;
+        var distance = input * speed * Time.deltaTime;
+        var targetPosition = transform.position + Vector3.up * distance;
+
+        playerRigidbody.MovePosition(targetPosition);
     }
 }
