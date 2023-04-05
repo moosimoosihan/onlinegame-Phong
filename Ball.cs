b030bc4b2b7915c5866bfd217276ea2bd490fe88
moosimoosihan <92845402+moosimoosihan@users.noreply.github.com>
2023-04-05 17:44:02 +0900

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -1,4 +1,4 @@
-﻿using Photon.Pun;
+using Photon.Pun;
 using UnityEngine;
 
 public class Ball : MonoBehaviourPun
@@ -11,6 +11,26 @@ public class Ball : MonoBehaviourPun
     
     private void FixedUpdate()
     {
+        // 방장이 아닌경우 || 플레이어가 2보다 적을 경우
+        if(!IsMasterClientLocal || PhotonNetwork.PlayerList.Length < 2) return;
 
+        var distance = speed * Time.deltaTime;
+        var hit = Physics2D.Raycast(transform.position, direction, distance);
+
+        if(hit.collider != null){
+
+            var goalPost = hit.collider.GetComponent<Goalpost>();
+            if(goalPost != null){
+                if(goalPost.playerNumber == 1){
+                    GameManager.Instance.AddScore(2,1);
+                } else if(goalPost.playerNumber == 2){
+                    GameManager.Instance.AddScore(1,1);
+                }
+            }
+            direction = Vector2.Reflect(direction, hit.normal);
+            direction += Random.insideUnitCircle * randomRefectionIntensity;
+        }
+
+        transform.position = (Vector2)transform.position + direction * distance;
     }
 }
